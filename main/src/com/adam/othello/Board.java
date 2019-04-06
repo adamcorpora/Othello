817e94f29334aaf7059dc35d56abdb9096e01f57
Adam Corpora <adam.corpora@gmail.com>
2019-04-05 23:49:33 -0400

    Cleaned up a few unneeded lines.

--- main/src/com/adam/othello/Board.java
+++ main/src/com/adam/othello/Board.java
@@ -12,7 +12,7 @@ public class Board {
 
 
     public Board (){
-        System.out.println("_|0|1|2|3|4|5");
+        System.out.println("\n" + "_|0|1|2|3|4|5");
         inner1.add("_|");
         inner1.add("_|");
         inner1.add("_|");
@@ -45,46 +45,23 @@ public class Board {
         if (Question.input == 0) {
             inner1.set(0,"X|");
         }
-        else {
-            inner1.set(0,"_|");
-        }
-        System.out.print(inner1.get(0));
-        if (Question.input == 1) {
+        else if (Question.input == 1) {
             inner1.set(1,"X|");
         }
-        else {
-            inner1.set(1,"_|");
-        }
-        System.out.print(inner1.get(1));
-        if (Question.input == 2) {
+        else if (Question.input == 2) {
             inner1.set(2,"X|");
         }
-        else {
-            inner1.set(2,"_|");
-        }
-        System.out.print(inner1.get(2));
-        if (Question.input == 3) {
+        else if (Question.input == 3) {
             inner1.set(3,"X|");
         }
-        else {
-            inner1.set(3,"_|");
-        }
-        System.out.print(inner1.get(3));
-        if (Question.input == 4) {
+        else if (Question.input == 4) {
             inner1.set(4,"X|");
         }
-        else {
-            inner1.set(4,"_|");
-        }
-        System.out.print(inner1.get(4));
-        if (Question.input == 5) {
+        else if (Question.input == 5) {
             inner1.set(5,"X|");
         }
-        else {
-            inner1.set(5,"_|");
-        }
-        System.out.println(inner1.get(5));
-
+        System.out.print(inner1.get(0) + inner1.get(1) + inner1.get(2) + inner1.get(3) + inner1.get(4) + inner1.get(5));
+        System.out.println("\n");
         return 0;
     }
 }
--- main/src/com/adam/othello/Main.java
+++ main/src/com/adam/othello/Main.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Current board state:");
+        System.out.println("_|0|1|2|3|4|5");
+        System.out.println("0|_|_|_|_|_|_|");
+        System.out.println("1|_|_|_|_|_|_|");
+        System.out.println("2|_|_|X|O|_|_|");
+        System.out.println("3|_|_|O|X|_|_|");
+        System.out.println("4|_|_|_|_|_|_|");
+        System.out.println("5|_|_|_|_|_|_|" + "\n");
+
         Question ask = new Question(); //calls the Question file
 
 
